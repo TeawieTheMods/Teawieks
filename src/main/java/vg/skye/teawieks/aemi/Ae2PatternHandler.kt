fde88340d638639c0a663d3aebbea86aed819e9c
Skye <me@skye.vg>
2023-11-17 20:28:50 +0900

    better AE2-EMI integration

--- src/main/java/vg/skye/teawieks/aemi/Ae2PatternHandler.kt
+++ src/main/java/vg/skye/teawieks/aemi/Ae2PatternHandler.kt
@@ -2,37 +2,25 @@ package vg.skye.teawieks.aemi
 
 import appeng.api.stacks.GenericStack
 import appeng.integration.modules.jeirei.EncodingHelper
-import appeng.menu.SlotSemantics
 import appeng.menu.me.items.PatternEncodingTermMenu
 import dev.emi.emi.api.recipe.EmiRecipe
 import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories
 import dev.emi.emi.api.recipe.handler.EmiCraftContext
 import dev.emi.emi.api.recipe.handler.StandardRecipeHandler
-import dev.emi.emi.api.stack.EmiIngredient
-import dev.emi.emi.api.stack.EmiStack
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.core.registries.Registries
-import net.minecraft.world.SimpleContainer
 import net.minecraft.world.inventory.Slot
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.crafting.Recipe
-import net.minecraft.world.item.crafting.RecipeManager
-import net.minecraftforge.registries.ForgeRegistries
 
 
 class Ae2PatternHandler<T: PatternEncodingTermMenu>: StandardRecipeHandler<T> {
-    private val creativeInputSource: List<Slot> = ForgeRegistries.ITEMS.map { s ->
-        val stack = ItemStack(s)
-        stack.count = 64
-        Slot(SimpleContainer(stack), 0, 0, 0)
-    }.toList()
-
     override fun getInputSources(handler: T): List<Slot> {
-        return creativeInputSource
+        return listOf()
+    }
+
+    override fun canCraft(recipe: EmiRecipe?, context: EmiCraftContext<T>?): Boolean {
+        return true
     }
 
     override fun getCraftingSlots(handler: T): List<Slot> {
-        return handler.getSlots(SlotSemantics.CRAFTING_GRID)
+        return listOf()
     }
 
     override fun supportsRecipe(recipe: EmiRecipe): Boolean {
@@ -42,23 +30,28 @@ class Ae2PatternHandler<T: PatternEncodingTermMenu>: StandardRecipeHandler<T> {
 
     override fun craft(recipe: EmiRecipe, context: EmiCraftContext<T>): Boolean {
         val menu = context.screenHandler
-        val recipeId = recipe.id ?: return false
-        val vanillaRecipe = menu.player.level().recipeManager.byKey(recipeId)
-
-        if (vanillaRecipe.isEmpty) {
-            return false
-        }
-
-        val items = recipe.inputs.stream().map { ingredient ->
-                if (ingredient.isEmpty) {
-                    listOf()
-                } else {
-                    ingredient.emiStacks.map {
-                        GenericStack.fromItemStack(it.itemStack)
-                    }
+        val items = recipe.inputs.map { ingredient ->
+            if (ingredient.isEmpty) {
+                listOf()
+            } else {
+                ingredient.emiStacks.map {
+                    GenericStack.fromItemStack(it.itemStack)
                 }
-            }.toList()
-        EncodingHelper.encodeCraftingRecipe(menu, vanillaRecipe.get(), items) { true }
+            }
+        }
+        when (recipe.category) {
+            VanillaEmiRecipeCategories.CRAFTING,
+            VanillaEmiRecipeCategories.STONECUTTING,
+            VanillaEmiRecipeCategories.SMITHING -> {
+                val recipeId = recipe.id ?: return false
+                val vanillaRecipe = menu.player.level().recipeManager.byKey(recipeId)
+                if (vanillaRecipe.isEmpty) {
+                    return false
+                }
+                EncodingHelper.encodeCraftingRecipe(menu, vanillaRecipe.get(), items) { true }
+            }
+            else -> EncodingHelper.encodeProcessingRecipe(menu, items, recipe.outputs.map { GenericStack.fromItemStack(it.itemStack) })
+        }
         return true
     }
 }
