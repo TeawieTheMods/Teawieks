bfc3f7a6f9309e3be2bb5805d2ec8781f9df0ca4
Skye <me@skye.vg>
2023-12-03 13:35:39 +0900

    whoops /nick feedback

--- src/main/java/vg/skye/teawieks/nickinator/RegisterCommands.kt
+++ src/main/java/vg/skye/teawieks/nickinator/RegisterCommands.kt
@@ -5,6 +5,7 @@ import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.ComponentArgument
 import net.minecraft.commands.arguments.MessageArgument
+import net.minecraft.network.chat.Component
 
 fun nickinatorRegisterCommands(dispatcher: CommandDispatcher<CommandSourceStack>) {
     dispatcher.register(
@@ -17,12 +18,23 @@ fun nickinatorRegisterCommands(dispatcher: CommandDispatcher<CommandSourceStack>
                         val user = ctx.source.playerOrException.uuid
                         val nickname = MessageArgument.getMessage(ctx, "nickname")
                         NickinatorData.getInstance()!!.set(user, nickname)
+                        ctx.source.sendSuccess({
+                            Component.translatable(
+                                "teawieks.command.nick.set",
+                                nickname
+                            )
+                        }, false)
                         0
                     }
             )
             .executes { ctx ->
                 val user = ctx.source.playerOrException.uuid
                 NickinatorData.getInstance()!!.set(user, null)
+                ctx.source.sendSuccess({
+                    Component.translatable(
+                        "teawieks.command.nick.clear"
+                    )
+                }, false)
                 0
             }
     )
@@ -36,12 +48,23 @@ fun nickinatorRegisterCommands(dispatcher: CommandDispatcher<CommandSourceStack>
                     val user = ctx.source.playerOrException.uuid
                     val nickname = ComponentArgument.getComponent(ctx, "nickname")
                     NickinatorData.getInstance()!!.set(user, nickname)
+                    ctx.source.sendSuccess({
+                        Component.translatable(
+                            "teawieks.command.nick.set",
+                            nickname
+                        )
+                    }, false)
                     0
                 }
             )
             .executes { ctx ->
                 val user = ctx.source.playerOrException.uuid
                 NickinatorData.getInstance()!!.set(user, null)
+                ctx.source.sendSuccess({
+                    Component.translatable(
+                        "teawieks.command.nick.clear"
+                    )
+                }, false)
                 0
             }
     )
